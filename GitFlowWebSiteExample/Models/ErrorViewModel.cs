851ee443f25f96049a14f8d6882294ca4abf8e08
gotoa1234 <cap8825@gmail.com>
2023-11-14 15:24:28 +0800

    1. 最基本的網站代碼

---
+++ GitFlowWebSiteExample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GitFlowWebSiteExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
